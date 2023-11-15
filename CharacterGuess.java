54b5d2cbff548ee9d9ad3027de612d2597d437bd
lizziedelmonico <123976957+lizziedelmonico@users.noreply.github.com>
2023-11-15 13:46:09 -0500

    DUBS

--- CharacterGuess.java
+++ CharacterGuess.java
@@ -1,75 +1,148 @@
-import java.util.Scanner;
 import java.io.File;
+import java.util.Scanner;
+
 
 public class CharacterGuess {
-    private static DecisionTree tree = new DecisionTree("Human-ish?");
     
-    public void characterTree(){
-            tree.setLeft(new DecisionTree("Royalty?"));
-            tree.setRight(new DecisionTree("Animal?"));
-            tree.getLeft().setLeft(new DecisionTree("Queen/Princess?"));
-            tree.getLeft().setRight(new DecisionTree("Finn"));
-            tree.getLeft().getLeft().setLeft(new DecisionTree("Bright colors?"));
-            tree.getLeft().getLeft().getLeft().setRight(new DecisionTree("Marceline"));
-            tree.getLeft().getLeft().getLeft().setLeft(new DecisionTree("Bubblegum"));
-            tree.getLeft().getLeft().setRight(new DecisionTree("Cold?"));
-            tree.getLeft().getLeft().getRight().setLeft(new DecisionTree("Ice King"));
-            tree.getLeft().getLeft().getRight().setRight(new DecisionTree("Flame King"));
-            tree.getRight().setLeft(new DecisionTree("Common pet?"));
-            tree.getRight().getLeft().setLeft(new DecisionTree("Jake"));
-            tree.getRight().getLeft().setRight(new DecisionTree("Tree Trunks"));
-            tree.getRight().setRight(new DecisionTree("Smiling?"));
-            tree.getRight().getRight().setRight(new DecisionTree("Lumpy Space Princess"));
-            tree.getRight().getRight().setLeft(new DecisionTree("Food?"));
-            tree.getRight().getRight().getLeft().setLeft(new DecisionTree("Peppermint Butler"));
-            tree.getRight().getRight().getLeft().setRight(new DecisionTree("BMO"));
+    public static void runGame(DecisionTree tree) {
+        // Game
+        Scanner userInput = new Scanner(System.in);
+        String playAgain = "";
+
+        while (!playAgain.equals("no")) {
+            System.out.println("Think of a character!");
+            String path = "";
+
+            while (tree.followPath(path).getLeft() != null && tree.followPath(path).getRight() != null) {
+
+                if (path.equals("")) {
+                    System.out.println(tree.followPath(path).getData());
+                }
+
+                String response = userInput.nextLine();
+
+                if (response.equals("yes")) {
+                    path += "Y";
+                    if (tree.followPath(path).getLeft() == null && tree.followPath(path).getRight() == null) {
+                        break;
+                    }
+                    System.out.println(tree.followPath(path).getData());
+                } else if (response.equals("no")) {
+                    path += "N";
+                    if (tree.followPath(path).getLeft() == null && tree.followPath(path).getRight() == null) {
+                        break;
+                    }
+                    System.out.println(tree.followPath(path).getData());
+                }
+
+            }
+
+            String guessedCharacter = tree.followPath(path).getData();
+            System.out.println("Is your character " + guessedCharacter + "?");
+            String response = userInput.nextLine();
+
+            if (response.equals("yes")) {
+                System.out.println("I knew it!");
+            } 
+            else if (response.equals("no")) {
+                System.out.println("Uh oh.. I got it wrong");
+                System.out.println("Who was your character?");
+                String actual = userInput.nextLine();
+                System.out.println("Type a yes or no question that would distinguish between a " + actual + " and a " + tree.followPath(path).getData() + ".");
+                String actualAnimalQuestion = userInput.nextLine();
+                System.out.println("Would you answer yes to this question for " + actual);
+                String actualQA = userInput.nextLine();
+
+                // take path of original answer; replace it with new question, and use original
+                // answer and actual answer as the children of the question.
+                tree.followPath(path).setData(actualAnimalQuestion);
+                if (actualQA.equals("yes")) {
+                    tree.followPath(path).setLeft(new DecisionTree(actual));
+                    tree.followPath(path).setRight(new DecisionTree(guessedCharacter));
+                }
+                if (actualQA.equals("no")) {
+                    tree.followPath(path).setRight(new DecisionTree(actual));
+                    tree.followPath(path).setLeft(new DecisionTree(guessedCharacter));
+                }
+
+
+            }
+            System.out.println("Play again?");
+            playAgain = userInput.nextLine();
+        } 
+        userInput.close();
     }
 
-    public static DecisionTree fileTree(String filename){
-        Scanner file = null;
-        DecisionTree root = null;
-        try{
-            file = new Scanner(new File(filename));
-        } catch(Exception e){
-            System.err.println("Cannot locate file");
-            System.exit(-1);
-        }
-        String line = file.nextLine();
-        root = new DecisionTree(line.substring(1));
-        while(file.hasNextLine()){
-            line = file.nextLine();
-            String[] words = line.split("\\s+");
-            root.makeTreeInPath(words[0], line.substring(words[0].length() + 1));
-        }
-        file.close();
-        return root;
-    }
+    /* FOR MAIN, IF NO ARGS, DO HARD CODE TREE */
+
     public static void main(String[] args){
-        String start = "Y";
-    /* to keep track of position in binary tree use breadth-frist approach and make each node a queue + iterate through(USE FOR PHASE 3 NOT HERE)*/
-       Scanner scanner = new Scanner(System.in);
-       DecisionTree current = tree;
-       
-        System.out.println(current);
+        if(args.length == 0){
+            DecisionTree tree = new DecisionTree("Human-ish?", null, null);
+                tree.setLeft(new DecisionTree("Royalty?"));
+                tree.setRight(new DecisionTree("Animal?"));
+                tree.getLeft().setLeft(new DecisionTree("Queen or Princess?"));
+                tree.getLeft().setRight(new DecisionTree("Finn"));
+                tree.getLeft().getLeft().setLeft(new DecisionTree("Bright colors?"));
+                tree.getLeft().getLeft().getLeft().setRight(new DecisionTree("Marceline"));
+                tree.getLeft().getLeft().getLeft().setLeft(new DecisionTree("Bubblegum"));
+                tree.getLeft().getLeft().setRight(new DecisionTree("Cold?"));
+                tree.getLeft().getLeft().getRight().setLeft(new DecisionTree("Ice King"));
+                tree.getLeft().getLeft().getRight().setRight(new DecisionTree("Flame King"));
+                tree.getRight().setLeft(new DecisionTree("Common pet?"));
+                tree.getRight().getLeft().setLeft(new DecisionTree("Jake"));
+                tree.getRight().getLeft().setRight(new DecisionTree("Tree Trunks"));
+                tree.getRight().setRight(new DecisionTree("Smiling?"));
+                tree.getRight().getRight().setRight(new DecisionTree("Lumpy Space Princess"));
+                tree.getRight().getRight().setLeft(new DecisionTree("Food?"));
+                tree.getRight().getRight().getLeft().setLeft(new DecisionTree("Peppermint Butler"));
+                tree.getRight().getRight().getLeft().setRight(new DecisionTree("BMO"));
 
-        while(current.getLeft() != null && current.getRight() != null){
-            String answers = scanner.next();
+            runGame(tree);
 
-            if(answers.equals("YES") || answers.equals("Yes") || answers.equals("yes") || answers.equals("Y")){
-                System.out.println(current.getLeft().getData());
-                current = current.getLeft();
-            }
-            if(answers.equals("NO") || answers.equals("No") || answers.equals("no") || answers.equals("N")){
-                System.out.println(current.getRight().getData());
-                current = current.getRight();
-            }
-            else{
-                System.out.println("I'm sorry.. I didn't quite understand that. Please try again!");
-                System.out.println(current);
+        }   
+        else{
+            String start = "Y";
+            String filename = args.toString();
+        
+            while(start.equalsIgnoreCase("Y")){
+            
+                DecisionTree currentTree = DecisionTree.fileTree(filename);
+                String treeRes = "";
+                String treeReses = "";
+                DecisionTree root = DecisionTree.fileTree(filename);
+
+                while(!currentTree.isLeaf()){
+                    boolean leafPass = false;
+                    while(!leafPass){
+                        try{
+                            Scanner question = new Scanner(System.in);
+                            System.out.println(currentTree.getData());
+                            treeRes = question.nextLine();
+                            treeReses += treeRes;
+                            currentTree = root.followPath(treeReses);
+                            leafPass = true;
+                        } catch(Exception e){
+                            System.out.println("please enter either Y or N");
+                            treeReses = treeReses.substring(0, treeReses.length() - treeRes.length());
+                        }
+                    }
+                }
+                System.out.println(currentTree.getData());
+                boolean whilePass = false;
+                while(!whilePass){
+                    Scanner question = new Scanner(System.in);
+                    System.out.println("Would you like to play again?");
+                    start = question.nextLine();
+                    if(!start.equalsIgnoreCase("Y") && !start.equalsIgnoreCase("N")){
+                        System.out.println("Please enter either Y or N");
+                    } else{
+                        whilePass = true;
+                    }
+                }
             }
         }
-
     }
 }
+
     
 
--- DecisionTree.java
+++ DecisionTree.java
@@ -1,3 +1,6 @@
+import java.util.Scanner;
+import java.io.File;
+
 /** 
  *  Class to represent a decision tree 
  */
@@ -35,28 +38,62 @@ public class DecisionTree extends BinaryTree<String> {
   /** manipulator */
   public void setRight(BinaryTree<String> right) {
     if (right instanceof DecisionTree) {
-      //this.left = left;
-      super.setLeft(right);
+      //this.right = right;
+      super.setRight(right);
     } else {
       throw new UnsupportedOperationException("Tried to add non-DecisionTree as child");}
     }
 
 
-    public String followPath(DecisionTree node, String answers){
-        for(int i = 0; i <= answers.length(); i++){
-            if(answers.charAt(i) == ('Y')){
-                String data = node.getLeft().getData();
-                return data;
-            }
-            if(answers.charAt(i) == ('N')){
-                String data = node.getRight().getData();
-                return data;
-            }
+    public DecisionTree followPath(String answers){
+        if(answers.length() == 0){
+          return this;
         }
-        System.out.println(data);
-        return data;
-        
+        if(answers.substring(0, 1).equals("Y")){
+          return getLeft().followPath(answers.substring(01));
+        }
+        if(answers.substring(0, 1).equals("N")){
+          return getRight().followPath(answers.substring(01));
+        }
+        else{
+          throw new UnsupportedOperationException("Not a Y or N found in the path");
+        }
     }
+
+    public void createTree(String input, String question){
+      String input1 = input.substring(0, input.length() - 1);
+      DecisionTree finalTree = followPath(input1);
+      if(input.substring(input.length() - 1).equalsIgnoreCase("Y")){
+        DecisionTree newTree = new DecisionTree(question);
+        finalTree.setLeft(newTree);
+      } else if(input.substring(input.length() - 1).equalsIgnoreCase("N")){
+        DecisionTree newTree = new DecisionTree(question);
+        finalTree.setRight(newTree);
+      } else{
+        System.out.println(input);
+        throw new IllegalArgumentException("You must only type in Y or N");
+      }
+    }
+
+    public static DecisionTree fileTree(String filename){
+      Scanner file = null;
+      DecisionTree root = null;
+      try{
+          file = new Scanner(new File(filename));
+      } catch(Exception e){
+          System.err.println("Cannot locate file");
+          System.exit(-1);
+      }
+      String line = file.nextLine();
+      root = new DecisionTree(line.substring(1));
+      while(file.hasNextLine()){
+          line = file.nextLine();
+          String[] words = line.split("\\s+");
+          root.createTree(words[0], line.substring(words[0].length() + 1));
+      }
+      file.close();
+      return root;
+  }
 }
 
 
